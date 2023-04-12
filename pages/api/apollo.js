3df9d70e07b2dbaefe61c0c2bdf6b749d9015fc5
Om <om@webninjaz.com>
2023-04-12 16:36:21 +0530

    env2

--- pages/api/apollo.js
+++ pages/api/apollo.js
@@ -2,7 +2,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"; 
 
 export const client = new ApolloClient({
-    uri:`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/graphql`,
+    // uri:`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/graphql`,
+    uri:`https://ibportfolio-api.shubhisaran.repl.co/graphql`,
     cache: new InMemoryCache(),
 })
 
